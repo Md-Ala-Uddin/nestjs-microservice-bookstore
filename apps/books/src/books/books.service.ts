7d216d252c97f457a40a88f8a00354cadaead862
MD. ALA UDDIN <pial469048@gmail.com>
2025-07-16 01:25:03 +0600

    feat(books): refactor methods in book service in the book app

--- apps/books/src/books/books.service.ts
+++ apps/books/src/books/books.service.ts
@@ -1,26 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { BookDto } from './dto/book.dto';
 
 @Injectable()
 export class BooksService {
+  private books: BookDto[] = [
+    {
+      id: 1,
+      title: 'Title 1',
+      author: 'Author 1',
+      rating: 3.9,
+    },
+    {
+      id: 2,
+      title: 'Title 2',
+      author: 'Author 2',
+      rating: 4.5,
+    },
+  ];
   create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+    const newBook: BookDto = {
+      ...createBookDto,
+      id: this.books.length + 1,
+    };
+
+    return newBook;
   }
 
   findAll() {
-    return `This action returns all books`;
+    return this.books;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} book`;
+    return this.books.find((book: BookDto) => book.id === id);
   }
 
   update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+    const bookIndex = this.books.findIndex((book: BookDto) => book.id === id);
+    if (bookIndex === -1) {
+      return null;
+    }
+    const updatedBook: BookDto = {
+      ...this.books[bookIndex],
+      ...updateBookDto,
+      id: this.books[bookIndex].id,
+    };
+    this.books.splice(bookIndex, 1, updatedBook);
+    return updatedBook;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} book`;
+    const bookIndex = this.books.findIndex((book: BookDto) => book.id === id);
+    if (bookIndex === -1) {
+      return null;
+    }
+    const removedBook = this.books.splice(bookIndex, 1)[0];
+    return removedBook;
   }
 }
